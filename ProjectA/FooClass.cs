a0a40281318e98863f2c28f0f310ca7b50cf5ef5
Elia Dal Santo <elia.dal.santo@e-projectsrl.it>
2020-05-27 16:36:17 +0200

    Add project files.

---
+++ ProjectA/FooClass.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectA
+{
+    public class FooClass
+    {
+        public static void Foo(string whatever)
+        {
+            //Do something
+            return;
+        }
+
+        public static void Foo(ReportViewer rv)
+        {
+            //Do something
+            return;
+        }
+    }
+}
---
+++ ProjectB/Program.cs
@@ -0,0 +1,17 @@
+﻿using ProjectA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FooClass.Foo();
+        }
+    }
+}
